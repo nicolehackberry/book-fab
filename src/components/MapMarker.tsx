8fe7ddfb02abd7314c220c0a1f0f6ef2ad2ca6a4
nicolehackberry <nicole.lopez@hackberry.se>
2022-05-29 00:49:22 +0300

    Added more styles and logs

--- src/components/MapMarker.tsx
+++ src/components/MapMarker.tsx
@@ -1,32 +1,35 @@
-import React, { FC } from 'react';
-import { Marker } from 'react-native-maps';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import React, { FC } from "react";
+import { Marker } from "react-native-maps";
+import Icon from "react-native-vector-icons/FontAwesome";
 import { NativeStackNavigationProp } from "react-native-screens/native-stack";
 
-import { ICreatorsData } from '../screens/HomeScreen';
+import { ICreatorsData } from "../screens/HomeScreen";
 import { primaryColor } from "../utils/Colors";
 
 interface IMapMarker {
-    data: ICreatorsData;
-    navigation: NativeStackNavigationProp<any, any>;
+  data: ICreatorsData;
+  navigation: NativeStackNavigationProp<any, any>;
 }
 
-const MapMarker: FC<IMapMarker> = ({ data, navigation}) => {
-    
-    return (
-        <Marker
-          coordinate={{ latitude: data.userLocation.latitude, longitude: data.userLocation.longitude }}
-          pinColor="#8fd9a8"
-          onPress={() => {
-            console.log('TAG pressed pin with this info: ', data);
-            navigation.navigate('CreatorsScreen')
-          }}
-        >
-
-          <Icon name={'map-pin'} size={24} color={primaryColor} />
-          
-        </Marker>
-    );
+const MapMarker: FC<IMapMarker> = ({ data, navigation }) => {
+  console.log('TAG DATATATTA: ', data);
+  
+  return (
+    <Marker
+      coordinate={{
+        latitude: data.userLocation.latitude,
+        longitude: data.userLocation.longitude,
+      }}
+      pinColor="#8fd9a8"
+      onPress={() => {
+        navigation.navigate("CreatorsScreen", {
+          creatorData: data,
+        });
+      }}
+    >
+      <Icon name={"map-pin"} size={24} color={primaryColor} />
+    </Marker>
+  );
 };
 
 export default MapMarker;
--- src/screens/CreatorsScreen.tsx
+++ src/screens/CreatorsScreen.tsx
@@ -1,21 +1,145 @@
-import React, { FC } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { FC, useRef } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Dimensions,
+  Image,
+  ScrollView,
+  FlatList,
+  Animated,
+} from "react-native";
 
-const CreatorsScreen: FC = () => {
-    return (
-        <View style={styles.container}>
-            <Text>Creators Screen</Text>
-        </View>
-    );
+import Paginator from "../components/Paginator";
+import { ICreatorsData } from "./HomeScreen";
+
+const initialState: ICreatorsData = {
+  description: "",
+  email: "",
+  expertise: "",
+  id: "",
+  name: "",
+  socialMedia: {
+    facebook: "",
+    instagram: "",
+    tiktok: "",
+  },
+  test: false,
+  userLocation: {
+    latitude: 0,
+    longitude: 0,
+  },
+};
+
+const dataArray = [
+  {
+    image: require("../assets/nailstwo.jpg"),
+  },
+  {
+    image: require("../assets/nailstwo.jpg"),
+  },
+  {
+    image: require("../assets/nailstwo.jpg"),
+  },
+  {
+    image: require("../assets/nailstwo.jpg"),
+  },
+  {
+    image: require("../assets/nailstwo.jpg"),
+  },
+];
+
+const RenderItem: FC<any> = ({ item }: any) => {
+  return (
+    <View style={styles.flatlistContainer}>
+      <Image style={styles.flatlistImage} source={item.image} />
+    </View>
+  );
+};
+
+const CreatorsScreen = (props: any) => {
+  const creatorsData = props.route.params.creatorData;
+
+  const scrollX = useRef(new Animated.Value(0)).current;
+  const viewConfig = useRef({ viewAreaCoveragePercentThreshold: 50 }).current;
+
+  return (
+    <ScrollView style={styles.container}>
+      <Image
+        style={styles.imageConatiner}
+        source={require("../assets/nailstwo.jpg")}
+      />
+
+      <View style={styles.descriptionContainer}>
+        <Text style={styles.titleText}>{creatorsData.expertise}</Text>
+        <Text style={styles.underText}>{creatorsData.name}</Text>
+        <Text style={styles.descriptionText}>{creatorsData.description}</Text>
+
+        <Text style={styles.underText}>{creatorsData.email}</Text>
+      </View>
+
+      <View style={{ flex: 3, alignItems: "center" }}>
+        <FlatList
+          data={dataArray}
+          renderItem={({ item }) => <RenderItem item={item} />}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          pagingEnabled
+          bounces={false}
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+            {
+              useNativeDriver: false,
+            }
+          )}
+          scrollEventThrottle={32}
+          viewabilityConfig={viewConfig}
+        />
+        <Paginator data={dataArray} scrollX={scrollX} />
+      </View>
+    </ScrollView>
+  );
 };
 
 export default CreatorsScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 15,
-        justifyContent: 'center',
-        height: 500,
-        backgroundColor: 'beige',
-    }
-})
+  container: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+  descriptionContainer: {
+    padding: 32,
+  },
+  imageConatiner: {
+    height: 300,
+    width: "100%",
+  },
+  titleText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    fontFamily: "Cochin",
+    paddingBottom: 16,
+  },
+  underText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    fontFamily: "Cochin",
+  },
+  descriptionText: {
+    fontSize: 12,
+    fontFamily: "Cochin",
+    paddingBottom: 16,
+  },
+  flatlistContainer: {
+    paddingBottom: 16,
+    paddingTop: 16,
+    width: Dimensions.get("window").width,
+  },
+  flatlistImage: {
+    flex: 0.7,
+    justifyContent: "center",
+    width: Dimensions.get("window").width,
+    height: 500,
+  },
+});
