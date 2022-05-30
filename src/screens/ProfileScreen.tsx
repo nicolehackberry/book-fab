1436d9e6b335a6cfe908b5a40e939b630a29c7a3
nicolehackberry <nicole.lopez@hackberry.se>
2022-05-30 16:17:40 +0300

    Added styles to profile screen

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,5 +1,13 @@
 import React, { FC, useContext } from "react";
-import { StyleSheet, Text, Button, Dimensions, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  Button,
+  Dimensions,
+  ScrollView,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { NativeStackNavigationProp } from "react-native-screens/native-stack";
 
 import { AuthContext } from "../contexts/AuthContext";
@@ -7,35 +15,76 @@ import MissingAuthScreen from "./MissingAuthScreen";
 
 interface IProfileScreen {
   navigation: NativeStackNavigationProp<any, any>;
-};
-
-interface IProtectedItems {
-    children: React.ReactNode;
-    navigation: NativeStackNavigationProp<any, any>,
 }
 
-const ProtectedItems: React.FC<IProtectedItems> = ({ children, navigation }) => {
+interface IProtectedItems {
+  children: React.ReactNode;
+  navigation: NativeStackNavigationProp<any, any>;
+}
+
+const profileList = ["General", "Profile", "Logout", "Contact us"];
+
+const ProtectedItems: React.FC<IProtectedItems> = ({
+  children,
+  navigation,
+}) => {
   const authContext = useContext(AuthContext);
   const authStatus = authContext?.isUserSignedIn;
 
-  if (authStatus === null) return <MissingAuthScreen navigation={navigation}/>;
+  if (authStatus === null) return <MissingAuthScreen navigation={navigation} />;
   return <>{children || null}</>;
 };
 
 const ProfileScreen: FC<IProfileScreen> = ({ navigation }) => {
-
-
   return (
     <ProtectedItems navigation={navigation}>
-      <ScrollView style={styles.container}>
-        <Text>Profile Screen</Text>
-        <Button
+      {/* <TouchableOpacity
+            style={[styles.button, { backgroundColor: 'blue' }]}
+            onPress={() => {
+              console.log("TAG pressing 1");
+            }}
+          >
+            <Text>General</Text>
+          </TouchableOpacity> */}
+
+      <View style={styles.button}></View>
+
+      <TouchableOpacity
+        style={[styles.button]}
+        onPress={() => {
+          console.log("TAG pressing 1");
+        }}
+      >
+        <Text>Profile</Text>
+      </TouchableOpacity>
+
+      <View style={[styles.button, { flex: 3 }]}></View>
+
+      <TouchableOpacity
+        style={[styles.button]}
+        onPress={() => {
+          console.log("TAG pressing 1");
+        }}
+      >
+        <Text>Contact us</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.button]}
+        onPress={() => {
+          console.log("TAG pressing 1");
+        }}
+      >
+        <Text>Logout</Text>
+      </TouchableOpacity>
+      <View style={styles.button}></View>
+
+      {/* <Button
           title="Press Me!"
           onPress={() => {
             navigation.navigate("HomeScreen");
           }}
-        />
-      </ScrollView>
+        /> */}
     </ProtectedItems>
   );
 };
@@ -44,9 +93,16 @@ export default ProfileScreen;
 
 const styles = StyleSheet.create({
   container: {
-    padding: 15,
-    backgroundColor: "green",
+    flex: 1,
+    paddingLeft: 32,
+    flexDirection: "column",
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height,
+  },
+  button: {
+    paddingLeft: 32,
+    width: "100%",
+    justifyContent: "center",
+    height: 60,
   },
 });
